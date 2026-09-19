3af3fe2aad4d736ee7bce661e219c1d3b7ded0be
Vanzv3 <ivanzv2011@gmail.com>
2026-09-19 15:45:22 +0200

    add.calc.py

---
+++ calc.py
@@ -0,0 +1,11 @@
+a = float(input("Write first numer: "))
+b = float(input("Write second number: "))
+
+print("summe:", a + b)
+print("minse:", a - b)
+print("Mult:", a * b)
+
+if b != 0:
+    print("twin:", a / b)
+else:
+    print("you can not do this")
